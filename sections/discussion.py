8bb18fa630fd2ca05ec3afd842b014d420cce7e1
hindAbida <118387588+hindAbida@users.noreply.github.com>
2025-07-14 22:21:32 +0100

    Update discussion.py

--- sections/discussion.py
+++ sections/discussion.py
@@ -5,9 +5,9 @@ def show():
     <div class="section" id="Discussion">
         <h2>📈 Discussion</h2>
         <ul style="font-size:18px;">
-            <li>Comprendre l’apport de l’IA et des méthodes numériques aux villes durables</li>
-            <li>Analyser les tendances scientifiques et technologiques</li>
-            <li>Cartographier les publications et brevets liés à l’ODD 11</li>
+            <li>Suivre la trajectoire des publications et brevets en analysant leurs citations pour identifier leur impact réel et futur</li>
+            <li>Détecter les domaines émergents et les ruptures technologiques autour de l’ODD 11</li>
+            <li>Analyser les liens entre production scientifique, innovation industrielle et politiques publiques</li>
         </ul>
     </div>
     """, unsafe_allow_html=True)
